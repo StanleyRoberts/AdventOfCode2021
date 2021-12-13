f6e317852ab7abed1110d11d903948f459f50a4c
StanleyRoberts <stanley.heronwood@gmail.com>
2021-12-13 17:24:25 +0000

    removed dive

--- advent_of_code/day9.py
+++ advent_of_code/day9.py
@@ -1,7 +1,7 @@
 with open("inputs/day9input.txt", "r") as file:
     hmap = [[int(char) for char in line.strip()] for line in file]
 
-def basin(i, j, dive=False):
+def basin(i, j):
     global lows
     adj = [x for x in [(i, j-1) if j>0 else None,
            (i, j+1) if j<len(hmap[0])-1 else None,
@@ -10,12 +10,10 @@ def basin(i, j, dive=False):
 
     if all([hmap[i][j] < hmap[x][y] for x, y in adj]):
         lows += 1 + hmap[i][j]
-        dive = True
     size = {(i, j)}
-    if dive:
-        for x,y in adj:
-            if hmap[x][y] > hmap[i][j] and hmap[x][y]<9:
-                size = size.union(basin(x,y, True))
+    for x,y in adj:
+        if hmap[x][y] > hmap[i][j] and hmap[x][y]<9:
+            size = size.union(basin(x,y))
     return size
 
 
