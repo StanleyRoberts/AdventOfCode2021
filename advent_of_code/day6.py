0c5aeeeaf70e1fe2a544e6e0d8a16f1ed24a08a4
StanleyRoberts <stanley.heronwood@gmail.com>
2021-12-13 15:54:15 +0000

    added day6 solution

---
+++ advent_of_code/day6.py
@@ -0,0 +1,21 @@
+with open("inputs/day6input.txt", "r") as file:
+    lf = [int(i) for i in file.read().split(',')]
+
+list = [0]*9
+for i in lf:
+    list[i]+=1
+
+def rot(list):
+    temp = list[0]
+    for i in range(6):
+        list[i]=list[i+1]
+    list[6]=temp+list[7]
+    list[7]=list[8]
+    list[8]=temp
+    return list
+
+for i in range(256):
+    list = rot(list)
+    if i==79: print(sum(list))
+
+print(sum(list))
